141139d2ea95a4275acc5a8e82261c04534ec696
Edward Banner <edward.banner@gmail.com>
2021-12-10 18:14:03 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,86 @@
+import json
+import uuid
+
+
+def get_nb_cells_as_json(fname):
+    with open('numpy-numpy.ipynb') as f:
+        lines = [line for line in f.readlines()]
+        json_str = ''.join(lines)
+        json_dict = json.loads(json_str)
+
+    return json_dict['cells']
+
+
+def make_csv_from_nb(ipynb_fname):
+    nb_cells = get_nb_cells_as_json(ipynb_fname)
+    problems = get_problems(nb_cells)
+    problems_df = make_df(problems)
+    csv = output_to_csv(problems_df)
+    return csv
+
+
+def get_ith_problem_cells(nb_cells, i):
+    """
+
+    # input
+    nb_cells: notebook cells
+    i: the number of problem set you want
+
+    """
+    return nb_cells[i:i+5]
+
+
+def get_problems(nb):
+    nb_problems = len(nb) // 5
+    problems = []
+    for i in range(nb_problems):
+        problem_cells = get_ith_problem_cells(nb, i)
+        problem = make_problem(problem_cells)
+        problems.append(problem)
+    return problems
+
+
+def get_id():
+    """
+
+    # output
+    string of length 32 with lowercase letters and numbers
+
+    """
+    return str(uuid.uuid4()).replace('-', '')
+
+
+def get_title(problem_cells):
+    return ''.join(problem_cells[1]['source'])
+
+
+def get_placeholder(problem_cells):
+     return ''.join(problem_cells[2]['source'])
+
+
+def get_content(problem_cells):
+    return ''.join(problem_cells[4]['source'])
+
+
+def get_output(problem_cells):
+    output_lines = problem_cells[4]['outputs'][0]['text']
+    # strip newline off last line
+    output_lines[-1] = output_lines[-1].rstrip()
+    output = ''.join(output_line for output_line in output_lines)
+    return output
+
+
+def make_problem(problem_cells):
+    id = get_id()
+    title = get_title(problem_cells)
+    placeholder = get_placeholder(problem_cells)
+    content = get_content(problem_cells)
+    output = get_output(problem_cells)
+    problem = {
+        'id': id,
+        'title': title,
+        'placeholder': placeholder,
+        'content': content,
+        'output': output
+    }
+    return problem
---
+++ test_sample.py
@@ -0,0 +1,447 @@
+from main import (get_id, get_ith_problem_cells, get_nb_cells_as_json,
+                  make_csv_from_nb, make_problem)
+
+
+def test_full():
+    input = 'numpy-numpy.ipynb'
+    expected = """id,title,placeholder,content,output^M
+0622e7b5a2cd4df09c26203dbb0cda20,"Create a zero vector ""v"" of size 10","import numpy as np
+
+# Your code here
+print(v)","import numpy as np
+
+v = np.zeros(10)
+print(v)",[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]^M
+eca6fe43d8624c4c99edcffa58edade7,"Set ""yst"" to yesterday's date","import numpy as np
+
+yst = None;
+# Your code here
+print(yst)","import numpy as np
+
+yst = None;
+yst = np.datetime64('today') - np.timedelta64(1)
+print(yst)",2021-12-09"""
+    output = make_csv_from_nb(input)
+    assert output == expected
+
+
+def test_get_nb_cells_as_json():
+    input = 'numpy-numpy.ipynb'
+
+    expected = [
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "# 1"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Create a zero vector \"\"v\"\" of size 10"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 9,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "None\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "v = None\n",
+                "# Your code here\n",
+                "print(v)"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Solution"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 10,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "v = np.zeros(10)\n",
+                "print(v)"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "# 2"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Set \"yst\" to yesterday's date"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 11,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "None\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "yst = None\n",
+                "# Your code here\n",
+                "print(yst)"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 12,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "2021-12-09\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "yst = None\n",
+                "yst = np.datetime64('today') - np.timedelta64(1)\n",
+                "print(yst)"
+            ]
+        }
+    ]
+
+    output = get_nb_cells_as_json(input)
+    assert output == expected
+
+
+def test_get_ith_problem_cells():
+    nb_cells = [
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "# 1"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Create a zero vector \"\"v\"\" of size 10"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 9,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "None\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "v = None\n",
+                "# Your code here\n",
+                "print(v)"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Solution"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 10,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "v = np.zeros(10)\n",
+                "print(v)"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "# 2"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Set \"yst\" to yesterday's date"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 11,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "None\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "yst = None\n",
+                "# Your code here\n",
+                "print(yst)"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 12,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "2021-12-09\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "yst = None\n",
+                "yst = np.datetime64('today') - np.timedelta64(1)\n",
+                "print(yst)"
+            ]
+        }
+    ]
+
+    expected = [
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "# 1"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Create a zero vector \"\"v\"\" of size 10"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 9,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "None\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "v = None\n",
+                "# Your code here\n",
+                "print(v)"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Solution"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 10,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "v = np.zeros(10)\n",
+                "print(v)"
+            ]
+        },
+    ]
+
+    first_set = 0
+    first_ps = get_ith_problem_cells(nb_cells, first_set)
+
+    assert first_ps == expected
+
+
+def test_get_id():
+    id = get_id()
+    print(id)
+    for char in id:
+        assert char in 'abcdefghijklmnopqrstuvwxyz' or char in '0123456789'
+
+
+def test_make_problem():
+    problem_cells = [
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "# 1"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Create a zero vector \"\"v\"\" of size 10"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 9,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "None\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "v = None\n",
+                "# Your code here\n",
+                "print(v)"
+            ]
+        },
+        {
+            "cell_type": "markdown",
+            "metadata": {},
+            "source": [
+                "Solution"
+            ]
+        },
+        {
+            "cell_type": "code",
+            "execution_count": 10,
+            "metadata": {},
+            "outputs": [
+                {
+                "name": "stdout",
+                "output_type": "stream",
+                "text": [
+                "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]\n"
+                ]
+                }
+            ],
+            "source": [
+                "import numpy as np\n",
+                "\n",
+                "v = np.zeros(10)\n",
+                "print(v)"
+            ]
+        },
+    ]
+
+    expected = {
+        'id': id,
+        'title': """Create a zero vector ""v"" of size 10""",
+        'placeholder': """import numpy as np
+
+v = None
+# Your code here
+print(v)""",
+        'content': """import numpy as np
+
+v = np.zeros(10)
+print(v)"""
+,
+        'output': "[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]"
+    }
+
+    problem = make_problem(problem_cells)
+    
+    assert problem['title'] == expected['title']
+    print("problem['output']")
+    print(problem['output'])
+    print("expected['output']")
+    print(expected['output'])
+    assert problem['placeholder'] == expected['placeholder']
+    assert problem['content'] == expected['content']
+    assert problem['output'] == expected['output']
